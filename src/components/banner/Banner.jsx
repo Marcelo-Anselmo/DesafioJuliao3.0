d4690d17690557ea6d0c28a8b7801a247268f804
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-12-18 20:31:48 -0300

    feat:{API-NEWS}

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './index.scss'
+import React from "react";
+import { useState } from "react";
+import "./index.scss";
 
 const Banner = () => {
   return (
-    <div className='banner'>
-        <img src="./logo.svg" alt="" />
-        <div className='banner__get'>
-            <h1>Esteja por dentro das notícias de todo Brasil, em tempo Real!</h1>
-            <button>Fique por dentro!</button>
-        </div>
+    <div className="banner">
+      <img src="./logo.svg" alt="" />
+      <div className="banner__get">
+        <h1>Esteja por dentro das notícias de todo Brasil, em tempo Real!</h1>
+        <button>Fique por dentro!</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import Banner from '../../components/banner/Banner'
+import { CardNews } from '../../components/cardNews/CardNews'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <Banner/>
+      <CardNews/>
     </div>
   )
 }
