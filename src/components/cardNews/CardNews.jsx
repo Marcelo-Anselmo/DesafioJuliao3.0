4adff1e3092d4f9c0724c64322981aef2d7df5ba
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-12-19 16:56:10 -0300

    feat:{cardNews}

--- src/components/cardNews/CardNews.jsx
+++ src/components/cardNews/CardNews.jsx
@@ -11,16 +11,20 @@ export const CardNews = () => {
       return <span>CARREGANDO..</span>;
     } else if (data) {
       return Object.values(data.articles).map((articles) => {
-        // const clubeCasa = data.sources[articles.source];
-        // const clubeVisitante = data.clubes[partida.clube_visitante_id];
         return (
           <>
             <div className="cardgeral">
-              <section className="cardjogo">
-                <div className="cardjogo__aside cardjogo__asideleft">
+              <section className="cardnews">
+                <div className="card">
                   <p>{articles.author}</p>
                   <h2>{articles.title}</h2>
-                  <a href={articles.url} target="_blank" rel="noreferrer">Clique para mais noticias</a>
+                  <a
+                    id="lastNews"
+                    href={articles.url}
+                    target="_blank"
+                    rel="noreferrer">
+                    Saiba mais
+                  </a>
                   <p>{articles.publishedAt}</p>
                 </div>
               </section>
