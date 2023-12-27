b051e7997074f1461eb7720fb556ae16a203f591
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-12-26 22:05:28 -0300

    test{FilterFunc}

--- src/components/cardNews/CardNews.jsx
+++ src/components/cardNews/CardNews.jsx
@@ -1,38 +1,40 @@
 import "./index.scss";
 import useFetch from "../../Hooks/useFetch";
+import { MOCK } from "../../MOCK/moc";
 
 export const CardNews = () => {
   const { data, isFetching } = useFetch(
     "https://desafio-julio3-0-backend.vercel.app/"
   );
 
-
   const renderContent = () => {
     if (isFetching) {
       return <span>CARREGANDO..</span>;
     } else if (data) {
-      console.log(data)
+      console.log(data);
       return Object.values(data.articles).map((articles, index) => {
-        return (
-          <div key={index}>
-            <div className="cardgeral">
-              <section className="cardnews">
-                <div className="card">
-                  <p>{articles.author}</p>
-                  <h2>{articles.title}</h2>
-                  <a
-                    id="lastNews"
-                    href={articles.url}
-                    target="_blank"
-                    rel="noreferrer">
-                    Saiba mais
-                  </a>
-                  <p>{articles.publishedAt}</p>
-                </div>
-              </section>
+        if (MOCK.following.includes(articles.author)) {
+          return (
+            <div key={index}>
+              <div className="cardgeral">
+                <section className="cardnews">
+                  <div className="card">
+                    <p>{articles.author}</p>
+                    <h2>{articles.title}</h2>
+                    <a
+                      id="lastNews"
+                      href={articles.url}
+                      target="_blank"
+                      rel="noreferrer">
+                      Saiba mais
+                    </a>
+                    <p>{articles.publishedAt}</p>
+                  </div>
+                </section>
+              </div>
             </div>
-          </div>
-        );
+          );
+        }
       });
     } else {
       return null;
