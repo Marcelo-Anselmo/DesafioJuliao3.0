a23c495ddcf32591fdc264874672e3977f95d1e6
Marcelo_Anselmo <121883911+Marcelo-Anselmo@users.noreply.github.com>
2023-12-30 11:11:13 -0300

    fix{routerFix_0.1}

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,57 +1,69 @@
-import { useState } from 'react'
-import './index.scss'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import "./index.scss";
+import { Link, useNavigate } from "react-router-dom";
 
-const Form = ({h3='Olá, bem vindo!' , action='Login', metodo = 'GET', linkto='/signup', doing = 'cadastre-se', dohave = 'Não possui uma conta?'}) => {
-    
-    const [user, setUser] = useState({username: null, password: null})
-    const navigate = useNavigate()
+const Form = ({
+  h3 = "Olá, bem vindo!",
+  action = "Login",
+  metodo = "GET",
+  linkto = "/signup",
+  doing = "cadastre-se",
+  dohave = "Não possui uma conta?",
+}) => {
+  const [user, setUser] = useState({ username: null, password: null });
+  const navigate = useNavigate();
 
-    const sendLogin = async (event) => {
-        event.preventDefault()
+  const sendLogin = async (event) => {
+    event.preventDefault();
 
-        
+    const data = {
+      username: user.username,
+      password: user.password,
+    };
 
-        const data = {
-            username: user.username,
-            password: user.password
-        }
+    console.log(data);
 
-        console.log(data)
+    navigate("/");
+  };
 
-        
-        navigate("/")
-        
-    }
+  return (
+    <div className=".form">
+      <form onSubmit={sendLogin} action="" method={metodo}>
+        <h3>{h3}</h3>
+        <label htmlFor="">Username</label>
+        <input
+          type="text"
+          required
+          placeholder="@username"
+          autoComplete="@username"
+          onChange={(e) => {
+            let username = e.target.value;
+            setUser({ ...user, username: username });
+          }}
+        />
 
+        <label htmlFor="">Password</label>
+        <input
+          type="password"
+          autoComplete="current-password"
+          required
+          placeholder="*******"
+          onChange={(e) => {
+            let password = e.target.value;
+            setUser({ ...user, password: password });
+            console.log(password);
+          }}
+        />
 
-    return (
-    <div className='.form'>
-        
-        <form onSubmit={sendLogin} action="" method={metodo}>
-            <h3>{h3}</h3>
-                <label htmlFor="">Username</label>
-                <input type="text" required placeholder='@username' autoComplete='@username' onChange={(e) => {
-                    let username = e.target.value
-                    setUser({...user, username: username})
-                }}/>
+        <button type="submit">{action}</button>
 
-                <label htmlFor="">Password</label>
-                <input type="password" autoComplete='current-password' required placeholder='*******' onChange={(e) => {
-                    let password = e.target.value
-                    setUser({...user, password: password})
-                    console.log(password)
-                }}/>
-
-                <button type='submit'>{action}</button>
-                
-                <div style={{textAlign: 'center'}}>
-                    <p>{dohave}</p>
-                    <a href={linkto}>{doing}</a>
-                </div>
-        </form>
+        <div style={{ textAlign: "center" }}>
+          <p>{dohave}</p>
+          <Link to={linkto}><a>{doing}</a></Link>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
